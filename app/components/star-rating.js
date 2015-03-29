c3499c4758c71dd816309c8e2a77bb0243d6a90a
WayneSan <waynesan@gmail.com>
2015-03-30 02:19:38 +0800

    Enhanced to generate the options depends the `maximum` property.

--- app/components/star-rating.js
+++ app/components/star-rating.js
@@ -12,6 +12,13 @@ export default Ember.Component.extend({
             },
         });
     },
+    ratings: function () {
+        var maximum = this.get('maximum') || 5;
+        var ratingMapper = function (i) {
+            return {text: (i+1).toString(), value: i+1};
+        };
+        return Array.apply(null, {length: maximum}).map(Function.call, ratingMapper);
+    }.property('maximum'),
     actions: {
         onSelect: function (value, text) {
             this.sendAction('onRating', value, text);
